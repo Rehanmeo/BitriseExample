0dcc202ffb9b2fb6dbdd28bb7376eca668e9ee51
Muhammad Rehan <mrehanmeo@gmail.com>
2022-09-07 04:50:03 +0500

    Initial Commit

---
+++ BitriseExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BitriseExample
+//
+//  Created by Muhammad Rehan on 9/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
